d2d39cbd84675b468e66099d95f9f1f3fb8239ae
Anatoly Bazarov <dissahc@protonmail.com>
2023-04-18 07:46:00 -0400

    add SIGALRM handler to refresh session

--- src/signals.rs
+++ src/signals.rs
@@ -1,14 +1,20 @@
 use std::{thread, process::exit};
-use signal_hook::{ consts::SIGINT, iterator::Signals};
+use signal_hook::{ consts::{SIGINT,SIGALRM}, iterator::Signals};
+use std::sync::mpsc::SyncSender;
 
-pub fn start() {
-    let mut signals = Signals::new(&[SIGINT]).expect("error creating signal handler");
+pub enum SignalMessage {
+    SessionExpired,
+}
+
+pub fn start(signal_tx: SyncSender<SignalMessage>) {
+    let mut signals = Signals::new(&[SIGINT,SIGALRM]).expect("error creating signal handler");
 
     thread::spawn(move || {
         for sig in signals.forever() {
             eprintln!("Received signal {:?}", sig);
             match sig {
                 SIGINT => exit(0),
+                SIGALRM => signal_tx.send(SignalMessage::SessionExpired).unwrap(),
                 _ => {},
             }
         }
